926ae90b25860ed99141bd8ba3a3fba548607f19
Rallx-pd <leonardovinicius693@gmail.com>
2024-10-31 18:14:12 -0300

    atualização

---
+++ assets/Js/script.js
@@ -0,0 +1,12 @@
+const readButton = document.getElementById("readButton");
+const card = document.querySelectorAll(".card-list .card");
+
+readButton.addEventListener("click", () => {
+  card.forEach((card, index) => {
+    if (index >= 8) { // Mostra itens a partir do 9º
+      card.style.display = "grid";
+    }
+  });
+  
+  readButton.style.display = "none"; // Esconde o botão após o clique
+});
